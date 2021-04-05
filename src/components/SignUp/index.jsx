1adb6adf8ba55701f92a386237f07e49aa16fadc
OlliePorter <ol.porter@me.com>
2021-04-05 21:43:57 +0100

    Add front end API request in signup form

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,47 +1,64 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const SignUp = () => {
-  const [ state, setState ] = useState({
-    firstName: '',
-    secondName: '',
-    email: '',
-    password: '',
-    picture: '',
-  });
+  const [ state, setState ] = useState(
+    { 
+      firstName: '',
+      secondName: '',
+      email: '',
+      password: '',
+    }
+  );
 
-  handleFirstNameChange = (e) => {
+  const handleFirstNameChange = (e) => {
     setState({
       firstName: e.target.value,
     });
   }
 
-  handleLastNameChange = (e) => {
+  const handleLastNameChange = (e) => {
     setState({
       secondName: e.target.value,
     });
   }
 
-	handleEmailChange = (e) => {
+	const handleEmailChange = (e) => {
 		setState({
 			email: e.target.value,
 		});
 	}
 
-	handlePasswordChange = (e) => {
+	const handlePasswordChange = (e) => {
 		setState({
 			password: e.target.value,
 		});
 	}
 
-	handleAvatarChange = (e) => {
-		setState({
-			picture: e.target.files[0],
-		});
+  async function newUser() {
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: 'Api route for posting data to database',
+        data: { user: state },
+        crossdomain: true,
+      });
+      const { token } = response.data;
+      localStorage.setItem('jwt', token);	
+    } catch {
+      alert('THERE WAS A PROBLEM');
+    }
+  };
+
+  const onSubmitHandler = async (e) => {
+		e.preventDefault();
+		const { firstName, secondName, email, password } = state;
+		await newUser({firstName, secondName, email, password}).then(alert('Finished!'));
 	}
 
   return (
-    <form onSubmit={this.onSubmitHandler}>
-      <h1>{message}</h1>
+    <form onSubmit={onSubmitHandler}>
+      <h1>Sign Up</h1>
       <input
         onChange={handleFirstNameChange}
         type="text"
@@ -49,23 +66,20 @@ const SignUp = () => {
         placeholder="First name"
         required
       />
-
       <input
-        onChange={handleSecondNameChange}
+        onChange={handleLastNameChange}
         type="text"
         name="secondname"
         placeholder="Last name"
         required
       />
-
       <input
         onChange={handleEmailChange}
         type="email"
         name="email"
         placeholder="Email"
         required
-      />
-        
+      /> 
       <input
         onChange={handlePasswordChange}
         type="password"
@@ -73,13 +87,6 @@ const SignUp = () => {
         placeholder="Password"
         required
       />
-          
-      <input
-        onChange={handleAvatarChange}
-        type="file"
-        accept="/images/*"
-      />    
-
       <button type="submit">
         Create Account
       </button>
