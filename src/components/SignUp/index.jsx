f2b7a9235fdeac226c564daa78f151f8009908ba
Oliver Porter <oliverporter@olivers-macbook.home>
2021-04-05 15:47:51 +0100

    Add node & create sign up component

---
+++ src/components/SignUp/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+// import './App.css';
+
+const SignUp = () => {
+    const [ state, setState ] = useState(null);
+
+console.log('HERE', state);
+
+  return (
+    <h1>This is a sign up screen</h1>
+  );
+};
+
+export default SignUp;
