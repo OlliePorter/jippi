9221ad402b606a375939b64a4af5842e507d7242
OlliePorter <ol.porter@me.com>
2021-04-07 22:20:26 +0100

    Add css modules & first image to hero section

--- src/components/Home/Headline/index.jsx
+++ src/components/Home/Headline/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import styles from './headline.scss'
+import styles from './headline.module.scss'
 
 const Headline = () => {
   return (
     <div className={styles.cover}>
 			<p>This is text</p>
+			<img src="/conversation.png" alt="conversation" className={styles.image}></img>
     </div>
   );
 };
