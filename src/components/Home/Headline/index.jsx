5880550ddd3243f30ff2e4f9bb7dc15a7d3b5540
OlliePorter <ol.porter@me.com>
2021-04-07 21:33:16 +0100

    Yep this too

---
+++ src/components/Home/Headline/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './headline.scss'
+
+const Headline = () => {
+  return (
+    <div className={styles.cover}>
+			<p>This is text</p>
+    </div>
+  );
+};
+
+export default Headline;
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Headline from './Headline';
+
+const Home = () => {
+  return (
+    <>
+			<Headline/>
+    </>
+  );
+};
+
+export default Home;
