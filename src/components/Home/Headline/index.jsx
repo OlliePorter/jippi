eb0524bdc036edd33e08d409c249c751e53a2420
OlliePorter <ol.porter@me.com>
2021-04-08 22:42:21 +0100

    Fix text copy & add height to image

--- src/components/Home/Headline/index.jsx
+++ src/components/Home/Headline/index.jsx
@@ -10,7 +10,7 @@ const Headline = () => {
 			<div className={styles.innerContainer}>
 				<img src="/jippi_blue_square.png" alt="jippi logo" className={styles.logo}></img>
 				<p className={styles.title}>Find your tribe</p>
-				<p className={styles.subtitle}>An online adoption community for building relationships and alleviating loneness.</p>
+				<p className={styles.subtitle}>An online adoption community for building relationships and alleviating loneliness.</p>
 				<button>Sign up</button>
 			</div>
     </div>
