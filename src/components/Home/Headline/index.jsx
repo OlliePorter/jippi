32bfa7325c9100b64e23ca5851f0875b1864b737
OlliePorter <ol.porter@me.com>
2021-04-08 22:38:34 +0100

    Style headline component

--- src/components/Home/Headline/index.jsx
+++ src/components/Home/Headline/index.jsx
@@ -4,8 +4,15 @@ import styles from './headline.module.scss'
 const Headline = () => {
   return (
     <div className={styles.cover}>
-			<p>This is text</p>
-			<img src="/conversation.png" alt="conversation" className={styles.image}></img>
+			<div>
+				<img src="/conversation.png" alt="conversation" className={styles.image}></img>
+			</div>
+			<div className={styles.innerContainer}>
+				<img src="/jippi_blue_square.png" alt="jippi logo" className={styles.logo}></img>
+				<p className={styles.title}>Find your tribe</p>
+				<p className={styles.subtitle}>An online adoption community for building relationships and alleviating loneness.</p>
+				<button>Sign up</button>
+			</div>
     </div>
   );
 };
