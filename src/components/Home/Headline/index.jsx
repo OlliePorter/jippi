eb51713895c7dc855fa9599eecd4424783e98c8b
OlliePorter <ol.porter@me.com>
2021-04-09 16:58:51 +0100

    Fix spacing between image and text of cover & add see more

--- src/components/Home/Headline/index.jsx
+++ src/components/Home/Headline/index.jsx
@@ -3,17 +3,25 @@ import styles from './headline.module.scss'
 
 const Headline = () => {
   return (
-    <div className={styles.cover}>
-			<div>
-				<img src="/conversation.png" alt="conversation" className={styles.image}></img>
+		<div className={styles.cover}>
+			<div className={styles.innerCover}>
+				<div>
+					<img src="/conversation.png" alt="conversation" className={styles.image}></img>
+				</div>
+				<div className={styles.innerContainerOne}>
+					<img src="/jippi_blue_square.png" alt="jippi logo" className={styles.logo}></img>
+					<p className={styles.title}>Find your tribe</p>
+					<p className={styles.subtitle}>An online adoption community for building relationships and alleviating loneliness.</p>
+					<button>Sign up</button>
+				</div>
+    	</div>
+			<div className={styles.innerContainerTwo}>
+				<div className={styles.seeMore}>
+					<p>See more</p>
+					<svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 25 25" fill="none" stroke="#13b6b8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+				</div>
 			</div>
-			<div className={styles.innerContainer}>
-				<img src="/jippi_blue_square.png" alt="jippi logo" className={styles.logo}></img>
-				<p className={styles.title}>Find your tribe</p>
-				<p className={styles.subtitle}>An online adoption community for building relationships and alleviating loneliness.</p>
-				<button>Sign up</button>
-			</div>
-    </div>
+		</div>
   );
 };
 
