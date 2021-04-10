cfd78ad7ef06da0ab5476523d9843e633e8e6009
OlliePorter <ol.porter@me.com>
2021-04-10 15:54:48 +0100

    Add summary page

---
+++ src/components/Home/Summary/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './summary.module.scss'
+
+const Summary = () => {
+  return (
+		<div className={styles.container}>
+			<img src="/people.png" alt="people" className={styles.image}></img>
+			<div className={styles.text}>
+				<p className={styles.title}>Made for adoptees, by adoptees</p>
+				<p className={styles.subtitle}>Our app helps adopted people everywhere to form meaningful connections and provides each individual with the skills necessary to live a more fulfilling life.</p>
+			</div>
+		</div>
+  );
+};
+
+export default Summary;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Headline from './Headline';
+import Summary from './Summary';
 
 const Home = () => {
   return (
     <>
 			<Headline/>
+			<Summary/>
     </>
   );
 };
