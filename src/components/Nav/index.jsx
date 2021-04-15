e9804b0c2730be2ef54b173fb45f193932f0d6fa
OlliePorter <ol.porter@me.com>
2021-04-15 16:15:08 +0100

    Add active link state in nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -10,26 +10,41 @@ import styles from './nav.module.scss'
 import SignUp from '../SignUp';
 
 const Nav = () => {
-  const [isHide, setIsHide] = useState(false);  
+  const [isHidden, setIsHidden] = useState(false);  
+	const [isActive, setIsActive] = useState('home');
+
+	const setActiveLink = (linkName) => {
+		setIsActive(linkName);
+	}
+
+	const isActiveLink = (linkName) => {
+		return linkName === isActive;
+	}
 
 	const navClassNames = () => {
     return classNames(styles.outerNavContainer, {
-      [styles.hide]: isHide,
+      [styles.hide]: isHidden,
+    });
+  };
+	
+	const linkClassNames = (name) => {
+    return classNames(styles.jippiText, {
+      [styles.selected]: isActiveLink(name),
     });
   };
 	
   return (
 		<HashRouter>
 			<Headroom 
-				onPin={() => setIsHide(false)}
-				onUnpin={() => setIsHide(true)}
+				onPin={() => setIsHidden(false)}
+				onUnpin={() => setIsHidden(true)}
 			>
 				<ul className={navClassNames()}>
 					<img src="/jippi_blue.png"className={styles.jippiLogo} alt="Jippi logo"/>
 					<div className={styles.innerNavContainer}>
-						<li><NavLink exact to="/" className={styles.jippiText}>Home</NavLink></li>
-						<li><NavLink exact to="/about_us" className={styles.jippiText}>About Us</NavLink></li>
-						<li><NavLink exact to="/sign_up" className={styles.navButton}>Sign Up</NavLink></li>
+						<li><NavLink exact to="/" className={linkClassNames('home')} onClick={() => setActiveLink('home')}>Home</NavLink></li>
+						<li><NavLink exact to="/about_us" className={linkClassNames('about')} onClick={() => setActiveLink('about')}>About Us</NavLink></li>
+						<li><NavLink exact to="/sign_up" className={styles.navButton} onClick={() => setActiveLink('sign_up')}>Sign Up</NavLink></li>
 					</div>
 				</ul>
 			</Headroom>
