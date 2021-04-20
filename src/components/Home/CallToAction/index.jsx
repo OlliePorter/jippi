022c30ab568883221667d8748e526f4aa06a5f72
OlliePorter <ol.porter@me.com>
2021-04-20 11:03:45 +0100

    Change cta image height & text copy

--- src/components/Home/CallToAction/index.jsx
+++ src/components/Home/CallToAction/index.jsx
@@ -10,7 +10,7 @@ const CallToAction = () => {
 				</div>
 				<div className={styles.innerContainerOne}>
 					<p className={styles.title}>Sign up to Jippi today</p>
-					<p className={styles.subtitle}>Join a global community of adoptees and start walking a path to a heathier you.</p>
+					<p className={styles.subtitle}>Join a global community of adoptees each on their own path to a more authentic self.</p>
 					<button>Join Now</button>
 				</div>
     	</div>
