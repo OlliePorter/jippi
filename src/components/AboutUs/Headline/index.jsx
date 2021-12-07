d22c7ed4a2925755062dab2905a598c1ad1bef8a
Ollie Porter <ol.porter@me.com>
2021-12-07 14:13:19 +0000

    Text copy changes

--- src/components/AboutUs/Headline/index.jsx
+++ src/components/AboutUs/Headline/index.jsx
@@ -1,33 +1,41 @@
-import React from 'react';
-import styles from './headline.module.scss'
+import React from "react";
+import styles from "./headline.module.scss";
 
 const Headline = () => {
   return (
     <div className={styles.cover}>
-    <div className={styles.innerCover}>
-      <div>
-        <img src="/mother.png" alt="mother and baby" className={styles.image}></img>
+      <div className={styles.innerCover}>
+        <div>
+          <img
+            src="/mother.png"
+            alt="mother and baby"
+            className={styles.image}
+          ></img>
+        </div>
+        <div className={styles.innerContainerOne}>
+          <p className={styles.title}>Jippi</p>
+          <p className={styles.subtitleAlt}>
+            じっぴ (Japanese) <b>noun</b>
+          </p>
+          <p className={styles.subtitle}>Actual expense</p>
+        </div>
       </div>
-      <div className={styles.innerContainerOne}>
-        <p className={styles.title}>Jippi</p>
-        <p className={styles.subtitleAlt}>じっぴ (Japanese) <b>noun</b></p>
-        <p className={styles.subtitle}>Actual expense</p>
+      <div className={styles.innerContainerTwo}>
+        <div className={styles.seeMore}>
+          <p className={styles.title}>Our Story</p>
+          <p>
+            Our challenges and our experiences shape us into the people we are
+            today. Adoptees face more traumas and more challenges than most
+            other people. Statistically adopted people are 43% more likely to
+            develop substance abuse and 75% more likely to commit suicide than
+            non adopted people. The actual expense of adoption is deep,
+            ingrained mental scarring that can handicap adoptees in many
+            different ways. Our belief is that every adopted person can be
+            empowered to live life to the fullest through technology.
+          </p>
+        </div>
       </div>
     </div>
-    <div className={styles.innerContainerTwo}>
-      <div className={styles.seeMore}>
-        <p className={styles.title}>Our Story</p>
-        <p>
-          Our challenges and our experiences shape us into the people we are today. 
-          Adoptees face more traumas and more challenges than most other people. Statistically adopted people
-          are 43% more likely to develop substance abuse and 75% more likely to commit suicide than non adopted
-          people. The actual expense of adoption is deep, ingrained mental scarring that can handicap adoptees in many 
-          different ways. Our belief is that every adopted person can be empowered through technology, to live life
-          to the fullest.
-        </p>
-      </div>
-    </div>
-  </div>
   );
 };
 
--- src/components/AboutUs/VisionPurpose/index.jsx
+++ src/components/AboutUs/VisionPurpose/index.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import styles from './visionPurpose.module.scss'
+import React from "react";
+import styles from "./visionPurpose.module.scss";
 
 const VisionPurpose = () => {
   return (
-		<div className={styles.container}>
-			<div className={styles.card}>
-				<img src="/appreciate.png" alt="heart" className={styles.image}></img>
-				<p>Purpose</p>
-				<p>
-					Jippi’s purpose is to help support adopted people everywhere
-					with the many mental and social challenges that life
-					continuously throws at them.
-				</p>
-			</div>
-			<div className={styles.card}>
-				<img src="/rocket.png" alt="rocket" className={styles.image}></img>
-				<p>Vision</p>
-				<p>
-					We envision a positive social network dedicated to the
-					adoptee community which will equip every individual with
-					the skills necessary to live a more sociable, fulfilling
-					and dignified life. 
-				</p>
-			</div>
-		</div>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <img src="/appreciate.png" alt="heart" className={styles.image}></img>
+        <p>Purpose</p>
+        <p>
+          Jippi’s purpose is to help support adopted people everywhere with the
+          many mental and social challenges that life continuously throws at
+          them.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <img src="/rocket.png" alt="rocket" className={styles.image}></img>
+        <p>Vision</p>
+        <p>
+          Our vision is to build an ecosystem of tools dedicated to the adoptee
+          community which will equip every individual with the skills necessary
+          to live a more sociable, fulfilling and dignified life.
+        </p>
+      </div>
+    </div>
   );
 };
 
