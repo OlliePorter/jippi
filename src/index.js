0edc645040be19e500dbf17d82dbddce285dd0ab
OlliePorter <ol.porter@me.com>
2022-05-12 15:38:32 -0500

    Fix accidentally deleted fonts file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './fonts/FontsFree-Net-SF-Pro-Rounded-Bold.ttf';  
-import './fonts/FontsFree-Net-SF-Pro-Rounded-Regular.ttf';  
+import './fonts/Nunito-VariableFont_wght.ttf';  
 
 ReactDOM.render(
   <React.StrictMode>
