35e795e5eb37df87a6a6a34d667458c18f886796
OlliePorter <ol.porter@me.com>
2024-05-01 22:07:39 -0500

    Try different approach for mobile

--- src/components/PitchDeck/index.jsx
+++ src/components/PitchDeck/index.jsx
@@ -7,9 +7,15 @@ export default function PitchDeck() {
 
   return (
     <div className={styles.cover}>
-      <object data="/jippi_pitch_deck.pdf" type="application/pdf" width="100%" height="100%">
+      {/* <object data="/jippi_pitch_deck.pdf" type="application/pdf" width="100%" height="100%">
       <p>Tap <a href="/jippi_pitch_deck.pdf">here</a> for Jippi Pitch Deck</p>
-      </object>
+      </object> */}
+      <embed
+        src="/jippi_pitch_deck.pdf"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 }
