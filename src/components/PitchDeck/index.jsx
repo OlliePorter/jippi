fc92e97aeeb5616e1a3da9643ba31495360de8e5
OlliePorter <ol.porter@me.com>
2024-05-01 22:22:51 -0500

    Make Pitch Deck PDF open on page load

--- src/components/PitchDeck/index.jsx
+++ src/components/PitchDeck/index.jsx
@@ -1,21 +1,19 @@
+import { useEffect, useRef } from "react"; 
 import { useRouteError } from "react-router-dom";
 import styles from "./deck.module.scss";
 
 export default function PitchDeck() {
   // const error = useRouteError();
   // {error.statusText || error.message}
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    buttonRef.current.click();
+  }, []);
 
   return (
     <div className={styles.cover}>
-      {/* <object data="/jippi_pitch_deck.pdf" type="application/pdf" width="100%" height="100%">
-      <p>Tap <a href="/jippi_pitch_deck.pdf">here</a> for Jippi Pitch Deck</p>
-      </object> */}
-      <embed
-        src="/jippi_pitch_deck.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      />
+      <a ref={buttonRef} href="/jippi_pitch_deck.pdf" target="_blank">Download Jippi Pitch Deck PDF</a>
     </div>
   );
 }
