5ba36eb55d5a5c30354db23e291618f5d79a7fc8
OlliePorter <ol.porter@me.com>
2024-05-01 21:49:40 -0500

    Try adding iframe for pdf mobile support

--- src/components/PitchDeck/index.jsx
+++ src/components/PitchDeck/index.jsx
@@ -3,8 +3,12 @@ import styles from "./deck.module.scss";
 export default function PitchDeck() {
   return (
     <div className={styles.cover}>
-      <iframe src="/jippi_pitch_deck.pdf"></iframe>
-      <object data="/jippi_pitch_deck.pdf" type="application/pdf" width="100%" height="100%"/>
+      <iframe
+      id="pitchDeck"
+      src="/jippi_pitch_deck.pdf"
+      width="100%"
+      height="100%"
+      />
     </div>
   );
 }
