5980ac9b8ecf806ab27a0fdfaa5ceb99b70a83ee
OlliePorter <ol.porter@me.com>
2024-05-01 22:31:19 -0500

    Add download link

--- src/components/PitchDeck/index.jsx
+++ src/components/PitchDeck/index.jsx
@@ -5,15 +5,10 @@ import styles from "./deck.module.scss";
 export default function PitchDeck() {
   // const error = useRouteError();
   // {error.statusText || error.message}
-  const buttonRef = useRef(null);
-
-  useEffect(() => {
-    buttonRef.current.click();
-  }, []);
-
   return (
     <div className={styles.cover}>
-      <a ref={buttonRef} href="/jippi_pitch_deck.pdf" target="_blank">Open Jippi Pitch Deck PDF</a>
+      <object data="/jippi_pitch_deck.pdf" type="application/pdf" width="100%" height="100%"/>
+      <a href="/jippi_pitch_deck.pdf" target="_blank">Download Jippi Pitch Deck PDF</a>
     </div>
   );
 }
