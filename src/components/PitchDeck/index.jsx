10273e040f87720e8d4af3d5918fc10afdd94437
OlliePorter <ol.porter@me.com>
2024-05-01 22:36:36 -0500

    Add button styling

--- src/components/PitchDeck/index.jsx
+++ src/components/PitchDeck/index.jsx
@@ -5,10 +5,10 @@ import styles from "./deck.module.scss";
 export default function PitchDeck() {
   // const error = useRouteError();
   // {error.statusText || error.message}
+
   return (
     <div className={styles.cover}>
-      <object data="/jippi_pitch_deck.pdf" type="application/pdf" width="100%" height="100%"/>
-      <a href="/jippi_pitch_deck.pdf" target="_blank">Download Jippi Pitch Deck PDF</a>
+      <a className={styles.navButton} href="/jippi_pitch_deck.pdf" target="_blank">Download Jippi Pitch Deck PDF</a>
     </div>
   );
 }
