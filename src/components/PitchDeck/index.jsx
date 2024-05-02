7939c1ea2523e5055c57f4a467409040b4684052
OlliePorter <ol.porter@me.com>
2024-05-01 22:02:31 -0500

    Still try to get pdf to load on mobile

--- src/components/PitchDeck/index.jsx
+++ src/components/PitchDeck/index.jsx
@@ -1,14 +1,15 @@
+import { useRouteError } from "react-router-dom";
 import styles from "./deck.module.scss";
 
 export default function PitchDeck() {
+  // const error = useRouteError();
+  // {error.statusText || error.message}
+
   return (
     <div className={styles.cover}>
-      <iframe
-      id="pitchDeck"
-      src="/jippi_pitch_deck.pdf"
-      width="100%"
-      height="100%"
-      />
+      <object data="/jippi_pitch_deck.pdf" type="application/pdf" width="100%" height="100%">
+      <p>Tap <a href="/jippi_pitch_deck.pdf">here</a> for Jippi Pitch Deck</p>
+      </object>
     </div>
   );
 }
